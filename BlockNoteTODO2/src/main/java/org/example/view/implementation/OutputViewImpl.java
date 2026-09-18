4b6af779314dcc74e62158cef64936392c693a9f
grigor1986 <grigtaneev@gmail.com>
2026-09-18 15:10:49 +0300

    Класс для печати сообщений

---
+++ BlockNoteTODO2/src/main/java/org/example/view/implementation/OutputViewImpl.java
@@ -0,0 +1,10 @@
+package org.example.view.implementation;
+
+import org.example.view.OutputView;
+
+public class OutputViewImpl implements OutputView {
+    @Override
+    public void outputView(String text) {
+        System.out.println(text);
+    }
+}
